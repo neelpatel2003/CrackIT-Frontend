9a58dc5b936e8846de5fa2434c55c06aa226fb05
neelpatel123 <125110022+neelpatel2003@users.noreply.github.com>
2025-07-28 11:52:18 +0530

    Logout route added

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -4,7 +4,7 @@ const Loading = () => {
     return (
         <div className="loading-container">
             <video className="loading-video" autoPlay muted loop>
-                <source className='source-display' src="./src/utils/video/logo-animation.mp4" type="video/mp4" />
+                {/* <source className='source-display' src="./src/utils/video/logo-animation.mp4" type="video/mp4" /> */}
                 Your browser does not support the video tag.
             </video>
         </div>
